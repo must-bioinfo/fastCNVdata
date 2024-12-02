b827bd04507dc33d9661a28f5928440fe919c3fb
Gadea Cabrejas <gadea.cabrejas-saiz@u-paris.fr>
2024-12-02 15:59:29 +0100

    name changes

---
+++ R/scColon1.R
@@ -0,0 +1,20 @@
+#' scRNA-seq data from Human colorectal cancer
+#'
+#' Data from :  Spatially organized multicellular immune hubs in human colorectal
+#' cancer. Cell. 2021 Sep 2;184(18):4734-4752.e20. doi: 10.1016/j.cell.2021.08.003.
+#' Epub 2021 Aug 26. PMID: 34450029; PMCID: PMC8772395.
+#'
+#' @docType data
+#'
+#' @usage data(scColon1)
+#'
+#' @format An object of class Seurat.
+#'
+#' @keywords datasets
+#'
+#' @import Seurat
+#'
+#' @source https://pubmed.ncbi.nlm.nih.gov/34450029/
+
+
+"scColon1"
