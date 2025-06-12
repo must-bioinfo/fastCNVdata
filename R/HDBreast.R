16b4f233bd5d305f5d024814c9c2f7842202bb37
Gadea Cabrejas <gadea.cabrejas-saiz@u-paris.fr>
2025-06-12 11:50:55 +0200

    Added Visium HD data

---
+++ R/HDBreast.R
@@ -0,0 +1,19 @@
+#' Spatial transcriptomics HD data from Human breast cancer
+#'
+#' Data from https://www.10xgenomics.com/datasets
+#'
+#'
+#' @docType data
+#'
+#' @usage data(HDBreast)
+#'
+#' @format An object of class Seurat.
+#'
+#' @keywords datasets
+#'
+#' @import Seurat
+#'
+#' @source 10X Genomics datasets
+#'
+
+"HDBreast"
